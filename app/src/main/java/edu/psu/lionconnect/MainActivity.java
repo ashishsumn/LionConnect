df6519dd17a8acf983b9f39189a2a6a4d73b256c
Ashish <axs1875@psu.edu>
2020-03-30 20:03:47 -0400

    Sign up and Login

--- app/src/main/java/edu/psu/lionconnect/MainActivity.java
+++ app/src/main/java/edu/psu/lionconnect/MainActivity.java
@@ -1,35 +1,117 @@
 package edu.psu.lionconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.FirebaseDatabase;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
 
-//    final public FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmailField = findViewById(R.id.usernameET);
+        mPasswordField = findViewById(R.id.pwdET);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void buttonClick(View button) {
-        Intent intent = new Intent(this, RegisterationActivity.class);
-        startActivity(intent);
+
+    // Sign in
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String name = user.getDisplayName();
+                            Toast.makeText(MainActivity.this,name,
+                                    Toast.LENGTH_SHORT).show();
+                            forwardToHome();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Wrong EmailId or Password.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+//                        // [START_EXCLUDE]
+//                        if (!task.isSuccessful()) {
+//                            mStatusTextView.setText(R.string.auth_failed);
+//                        }
+//                        hideProgressBar();
+//                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
     }
 
-    public void loginClick(View view) {
+    // Validate email id and password fields
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    // Redirect to dashboard on successful login
+    private void forwardToHome(){
         Intent intent = new Intent(this, bottomNavActivity.class);
         startActivity(intent);
+    }
+
+    // Respond to button clicks
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.login_button) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.new_user_button) {
+            Intent intent = new Intent(this, RegistrationActivity.class);
+            startActivity(intent);
+        }
     }
 }

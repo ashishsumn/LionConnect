5988e0822af0de28b3750f0afded9fd1f3a57131
Ashish <axs1875@psu.edu>
2020-05-02 17:58:46 -0400

    User email search and follow/unfollow

--- app/src/main/java/edu/psu/lionconnect/SearchableActivity.java
+++ app/src/main/java/edu/psu/lionconnect/SearchableActivity.java
@@ -12,7 +12,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import java.util.Map;
@@ -34,38 +33,52 @@ public class SearchableActivity extends AppCompatActivity {
         if(Intent.ACTION_SEARCH.equals(intent.getAction())){
             String query = intent.getStringExtra(SearchManager.QUERY);
             Log.d(TAG, query);
-            searchFriends(query);
+
+            if(query.indexOf('@')!=-1){
+                String[] query_split_on_at = query.split("@",2);
+
+                if(query_split_on_at[1].equalsIgnoreCase("psu.edu")){
+                    // Search by emailId
+                    searchFriends(query, "email");
+                }
+            }
+            else{
+                // Search by UserName
+                searchFriends(query, "UserName");
+            }
         }
     }
 
-    private void searchFriends(String query){
+    // Search users with userId
+    private void searchFriends(String query, String mode){
 
         final Intent intent = new Intent(this, SearchActivity.class);
         intent.putExtra("search_result","True");
 
         fsInstance = FirebaseFirestore.getInstance();
         final CollectionReference usrRef = fsInstance.collection("users");
-//        Query res = usrRef.whereEqualTo("UserName",query);
-
-        usrRef.whereEqualTo("UserName",query).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        Log.d(TAG, mode+" "+query);
+        usrRef.whereEqualTo(mode,query).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             //no results returned
                             if(task.getResult().size()==0){
                                 Toast.makeText(SearchableActivity.this, "No search results found!!!", Toast.LENGTH_LONG).show();
+                                intent.putExtra("DisplayButtons", false);
                                 startActivity(intent);
                             }
 
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                Map<String, Object> userDetails = document.getData();
-
-                                int rec_index = 0;
+                                    Map<String, Object> userDetails = document.getData();
+                                intent.putExtra("UID",document.getId());
+//                                int rec_index = 0;
 
                                 for(Map.Entry<String,Object> entry:userDetails.entrySet()){
-                                    intent.putExtra(Integer.toString(rec_index), entry.getValue().toString());
-                                    rec_index++;
+//                                    intent.putExtra(Integer.toString(rec_index), entry.getValue().toString());
+//                                    rec_index++;
+                                    intent.putExtra(entry.getKey(), entry.getValue().toString());
                                 }
                                 Log.d(TAG,"forwarding to display...");
                                 startActivity(intent);
